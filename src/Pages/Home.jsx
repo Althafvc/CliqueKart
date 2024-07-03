3d36894440a865cd7804219bddee5ab43f373f85
Althaf vc <althafvellanchola46@gmail.com>
2024-07-03 19:22:05 +0530

    initial commit after implimenting navbar with responsiveness

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+
+function Home() {
+  return (
+<>
+<Navbar/>
+<div className="wrapper h-[600px] w-full bg-[#e5e7eb]"></div>
+</>    
+  )
+}
+
+export default Home
